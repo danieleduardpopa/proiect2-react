91d72d9423b840f600045e33c86c665bf226aa67
Daniel Popa <danieleduardpopa@gmail.com>
2023-01-26 10:01:13 +0200

    bootstrap install

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
