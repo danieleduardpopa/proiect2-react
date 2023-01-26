dd3e9b2e7a9f21c28cbf30d51b870942d4b4b2c1
Daniel Popa <danieleduardpopa@gmail.com>
2023-01-26 19:28:01 +0200

    footer header layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Mail } from '../assets/icons/mail.svg';
+import { ReactComponent as Phone } from '../assets/icons/phone.svg';
+import { ReactComponent as GitHub } from '../assets/icons/github.svg';
+import { ReactComponent as CopyRight } from '../assets/icons/copy-right.svg';
+import './Footer.css';
 
 function Footer() {
     return (
-        <div>
-            <Link to='/about'> About </Link>
-        </div>
+        <footer className='pt-3 bg-light'>
+            <div className='container-fluid container-min-max-width
+                                d-flex justify-content-between'>
+                <div>
+                    <h5>Link-uri:</h5>
+                    <p  className="mb-1">
+                        <Link to='/about'> About </Link>
+                    </p>
+                    <p className="m-0">
+                        <Link to='/terms-and-conditions'> Termeni si conditii </Link>
+                    </p>
+                </div>
+                <div>
+                    <h5>Contact</h5>
+                    <p className="mb-1">
+                        <a href='mailto:danieleduardpopa@gmail.com'>
+                        <Mail className="mr-1 mb-1 footer-icon"/> danieleduardpopa@gmail.com
+                        </a>
+                    </p>
+                    <p className="m-0">
+                        <Phone className="mr-1 footer-icon"/> +40 771 125 903
+                    </p>
+                </div>
+                <div>
+                    <h5>Contact</h5>
+                    <a href="https://github.com/semisemizeu">
+                        <p className="mb-1"><GitHub className="mr-1 mb-1 footer-icon"/> semisemizeu</p>
+                    </a>
+                </div>
+            </div>
+            <div className="copy-right text-center py-3">
+                <CopyRight />Daniel Popa 2022
+            </div>
+        </footer>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo2.png';
+import './Header.css';
+import { ReactComponent as ShoppingCart } from '../assets/icons/shopping-cart.svg';
 
 function Header() {
     return (
-        <div>
-            <Link to='/'> Home </Link>
-            <Link to='/login'> Login </Link>
+        <header  className="border-bottom mb-3">
+        <div className='header d-flex justify-content-between align-items-center container-fluid'>
+            <Link to='/' className="my-3"> 
+                <img src={logo} alt='logo' />
+            </Link>
+            <div>
+                <Link to='/login' className='h5'> Login </Link>
+                <ShoppingCart />
+            </div>
+
         </div>
+        </header>
     )
 }
 
