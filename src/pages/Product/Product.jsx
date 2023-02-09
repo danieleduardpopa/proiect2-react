f28a11922368fc022c4e5fee6983b8c89a29bc3f
Daniel Popa <danieleduardpopa@gmail.com>
2023-02-09 20:13:04 +0200

    redux folder and favorite product added

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,15 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import { connect } from 'react-redux';
-import { addToCart } from '../../redux/actions/cart';
+import { addToCart } from '../../redux/cart/CartActions';
 import { useParams } from 'react-router-dom';
 import Layout from '../../components/Layout/Layout';
 import products from '../../utils/products.json';
+import { addToFavorites, removeFromFavorites } from '../../redux/favorites/FavoritesActions';
+import fullHeart from '../../assets/icons/heart.svg';
+import heart from '../../assets/icons/heart-empty.svg';
 
 function Product(props) {
     const {productId} = useParams();
     const [ prodItem, setProdItem ] = useState({});
+    const isFavorite = props.favoriteProducts.find(prod => prod.id === prodItem.id);
     
-    console.log({prodItem});
     console.log(prodItem);
 
     useEffect(() => {
@@ -47,13 +50,48 @@ function Product(props) {
                     </div>
                     <div className='col-md-3 col-6 d-flex rounded flex-column my-auto mx-2 p-1 bg-light'>
                         <p className='border rounded text-center'>Pret: {prodItem.price} {prodItem.currency}</p>
-                        <button className='strong btn btn-dark'
+                        <button className='strong btn btn-dark mb-2'
                             onClick={() => props.addToCart({
                                 product: {...prodItem}
                             })}
                         >
                             Adauga in Cos
                         </button>
+                        
+                        {
+                            !isFavorite
+                                ? <div className='fav-item d-flex'>
+                                    <img
+                                    className='m-auto' 
+                                    src={heart} 
+                                    alt='heart-empty'
+                                    onClick={() => 
+                                        props.addToFavorites({
+                                        product: {
+                                            id: prodItem.id,
+                                            name: prodItem.name,
+                                            image: prodItem.image
+                                        }
+                                        })
+                                    } 
+                                    />
+                                </div>
+                                : <div className='fav-item d-flex flex-column justify-content-center align-item-center'>
+                                    <img
+                                    className='m-auto' 
+                                    src={fullHeart}
+                                    alt='full-heart' 
+                                    onClick={() => 
+                                        props.removeFromFavorites({
+                                        product: {
+                                            id: prodItem.id
+                                        }
+                                        })
+                                    }
+                                    />
+                                </div>
+                            }
+                        
                     </div>
                 </div>
             </div>
@@ -63,9 +101,17 @@ function Product(props) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        addToCart: (payload) => dispatch(addToCart(payload))
+        addToCart: (payload) => dispatch(addToCart(payload)),
+        addToFavorites: (payload) => dispatch(addToFavorites(payload)),
+        removeFromFavorites: (payload) => dispatch(removeFromFavorites(payload))
     }
 }
 
-export default connect(null, mapDispatchToProps)(Product);
+function mapStateToProps(state) {
+    return {
+        favoriteProducts: state.favorites.products
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
 
