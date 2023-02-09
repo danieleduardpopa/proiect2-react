3232a04b7b9b2f9b30d59e506ba4c26482a0c95f
Daniel Popa <danieleduardpopa@gmail.com>
2023-02-09 20:14:27 +0200

    redux folder change

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import cartReducer from "./reducers/cart";
-import userReducer  from "./reducers/user";
-import favoritesReducer from "./reducers/favorites";
+import cartReducer from "./cart/CartReducer";
+import userReducer  from "./user/UserReducer";
+import favoritesReducer from "./favorites/FavoritesReducer";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
