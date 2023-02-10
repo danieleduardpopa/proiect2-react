d892fbdb5f401e592c95ecd1f1e8b4bcbef6741d
Daniel Popa <danieleduardpopa@gmail.com>
2023-02-10 15:25:39 +0200

    firebase update

---
+++ src/configs/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBK3WAWuT2KlF8x-lA9cqAYMOEKz-NsA2k",
+  authDomain: "auth-dondon.firebaseapp.com",
+  projectId: "auth-dondon",
+  storageBucket: "auth-dondon.appspot.com",
+  messagingSenderId: "1017560031493",
+  appId: "1:1017560031493:web:7b05e834128da7de70576c"
+};
+
+
+
+export default firebaseConfig;
